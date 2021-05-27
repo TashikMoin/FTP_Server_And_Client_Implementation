e4e889da9571e36ac28577b71c8d36bc128f0eff
Tashik <k180142@nu.edu.pk>
2021-05-27 07:25:28 +0500

    added ftp server

--- Processes/client.py
+++ Processes/client.py
@@ -10,38 +10,17 @@ def recv_msg(client_socket):
     global recv_terminated
     global send_terminated
     recv_message = ""
-    while recv_message != 'q':
-        if send_terminated == False:
+    while recv_message != 'q' and send_terminated == False:
+        try:
             recv_message = str(client_socket.recv(1024).decode("utf-8"))
-            time.sleep(1)
             if recv_message == 'q':
                 client_socket.send(bytes('q', "utf-8"))
-                time.sleep(1)
                 print(f'\nConnection terminated.')
                 recv_terminated = True
                 break
-            elif recv_message == '...Recieving a file from peer...':
-                print("...Receiving File...")
-                file_name = str(client_socket.recv(1024).decode("utf-8"))
-                time.sleep(1)
-                print("Here 1")
-                file_size = str(client_socket.recv(1024).decode("utf-8"))
-                time.sleep(1)
-                print("Here 2")
-                data = str(client_socket.recv(int(file_size)).decode("utf-8"))
-                time.sleep(1)
-                print("Here 3")
-                print(f'peer # 2 received file_name --> {file_name}  file_size --> {file_size}  file_data --> {data}')
-                file_name = os.getcwd() + '/server_files/' + file_name
-                print(file_name)
-                file = open("abc.txt", "w")
-                file.write(data)
-                file.close()
-
-
             print(f'\nMessage recieved from peer  --->  {recv_message}\n>>')
-        else:
-            break
+        except socket.error as msg:
+            print(f'Socket Error: {msg}')
 
 def chat(client_socket, mode):
     os.system('clear')
@@ -53,23 +32,7 @@ def chat(client_socket, mode):
     print(f"Press 'q' to disconnect ")
     while send_msg != 'q' and recv_terminated == False:
         send_msg = input('\nEnter a message for peer : ')
-        if( send_msg == "upload file"):
-            file_name = input("Enter file name : ")
-            file = open(file_name, "r")
-            file_data = file.read()
-            file.close()
-            print(f'File data red  ---> {file_data}')
-            client_socket.send(bytes("file", "utf-8"))
-            time.sleep(1)
-            client_socket.send(bytes(file_name, "utf-8"))
-            time.sleep(1)
-            client_socket.send(bytes(str(len(file_data)), "utf-8"))
-            time.sleep(1)
-            client_socket.send(bytes(file_data, "utf-8"))
-            time.sleep(1)
-        else:
-            client_socket.send(bytes(send_msg, "utf-8"))
-            time.sleep(1)
+        client_socket.send(bytes(send_msg, "utf-8"))
     send_terminated = True
     if mode == 'wait':
         client_socket.close()
@@ -84,9 +47,7 @@ def connect_mode(client_socket):
     print(f'\n...Establishing Connection...')
     print(f'\n...Sending Connection Request...')
     client_socket.send(bytes(peer_ip, "utf-8"))
-    time.sleep(1)
     client_socket.send(bytes(peer_port, "utf-8"))
-    time.sleep(1)
     print(f'Connecting with {peer_ip} on port {peer_port}')
     time.sleep(5)
     print(f'\n...Connection Established...')
@@ -113,21 +74,59 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((sys.argv[1], int(sys.argv[2])) )
 while True:
     os.system('clear')
-    print(f'There are two modes for the client,')
+    print(f'There are three modes for the client,')
     print(f'1. Wait connection mode')
     print(f'2. Connect mode')
-    mode = input(f"Enter '1' for 'Wait connection mode' and '2' for 'Connect mode' : ")
+    print(f'3. FTP File upload')
+    print(f'4. FTP File download')
+    mode = input(f"Select mode : ")
     if mode == '1':
         client_socket.send(bytes('wait', "utf-8"))
-        time.sleep(1)
         client_ip = str(client_socket.recv(1024).decode("utf-8"))
-        time.sleep(1)
         client_port = int(input('Enter port number to start listening : '))
         wait_connection_mode(client_ip, client_port)
+        recv_terminated = False
+        send_terminated = False
+
     elif mode == '2':
         client_socket.send(bytes('connect', "utf-8"))
-        time.sleep(1)
         connect_mode(client_socket)
+        recv_terminated = False
+        send_terminated = False
+    
+    elif mode == '3':
+        client_socket.send(bytes('file upload', "utf-8"))
+        file_name = input('\nEnter file name :')
+        client_socket.send(bytes(file_name, "utf-8"))
+        with open(file_name, 'r') as file:
+            file_data = file.read()
+            client_socket.send(bytes(file_data, "utf-8"))
+            print(f'...Uploading file...')
+            time.sleep(5)
+            print(f'{file_name} is uploaded successfully')
+            file.close()
+
+
+
+    elif mode == '4':
+        client_socket.send(bytes('file download', "utf-8"))
+        file_name = input('\nEnter file name :')
+        client_socket.send(bytes(file_name, "utf-8"))
+        file_data = ""
+        client_socket.settimeout(5) # setting recv timeout
+        print(f'\n...Downloading file from the server...')
+        print(f'...Please wait...')
+        while True:
+            try: 
+                data = client_socket.recv(1024).decode("utf-8")
+                file_data = file_data + data
+            except socket.timeout:  # run when recv timeout expires
+                break
+        client_socket.settimeout(None)
+        with open(file_name, 'w') as file:
+            file.write(file_data)
+            file.close()
+
 
 
     
--- Processes/server.py
+++ Processes/server.py
@@ -5,20 +5,6 @@ import threading
 import time
 
 
-def file_transfer(first_peer, second_peer):
-    file_name = str(first_peer.recv(1024).decode("utf-8"))
-    time.sleep(1)
-    file_size = str(first_peer.recv(1024).decode("utf-8"))
-    time.sleep(1)
-    file_data = str(first_peer.recv(int(file_size)).decode("utf-8")) 
-    print(f'file_name --> {file_name} file_size --> {file_size} file_data --> {file_data}')
-    second_peer.send(bytes(file_name, "utf-8"))
-    time.sleep(1)
-    second_peer.send(bytes(file_size, "utf-8"))
-    time.sleep(1)
-    second_peer.send(bytes(file_data, "utf-8")) 
-
-
 
 isTerminated = False
 def API(first_peer, second_peer, current_peer_context):
@@ -27,33 +13,19 @@ def API(first_peer, second_peer, current_peer_context):
     while recv_message != 'q':
         if isTerminated == True:
             break
-        if current_peer_context == 'first':
-            recv_message = str(first_peer.recv(1024).decode("utf-8")) 
-            time.sleep(1)
-            print(f'Received data ----> {recv_message}')
-            # recv from client # 1
-            if recv_message == "file":
-                print('Recv message ---> file')
-                second_peer.send(bytes("...Recieving a file from peer...", "utf-8"))  
-                time.sleep(1)
-                file_transfer(first_peer, second_peer)
-            else:
+        try:
+            if current_peer_context == 'first':
+                recv_message = str(first_peer.recv(1024).decode("utf-8")) 
+                # recv from client # 1
                 second_peer.send(bytes(recv_message, "utf-8"))            
-            # send received message to client # 2
-        elif current_peer_context == 'second':
-            recv_message = str(second_peer.recv(1024).decode("utf-8")) 
-            time.sleep(1)
-            print(f'Received data ----> {recv_message}')
-            # recv from client # 2
-            if recv_message == "file":
-                print('Recv message ---> file')
-                first_peer.send(bytes("...Recieving a file from peer...", "utf-8")) 
-                time.sleep(1)
-                file_transfer(second_peer, first_peer)
-            else:
-                first_peer.send(bytes(recv_message, "utf-8"))     
-                time.sleep(1)         
-            # send received message to client # 1
+                # send received message to client # 2
+            elif current_peer_context == 'second':
+                recv_message = str(second_peer.recv(1024).decode("utf-8")) 
+                # recv from client # 2
+                first_peer.send(bytes(recv_message, "utf-8"))   
+                # send received message to client # 1
+        except socket.error as msg:
+            print(f'Socket Error: {msg}')
 
     isTerminated = True
     first_peer.close()
@@ -64,9 +36,11 @@ clients_list = []
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('127.0.0.1', int(sys.argv[1]) ))
 server_socket.listen(10)
-print("Server started listening on port # " + str(sys.argv[1]) )
+print("Server listening on port # " + str(sys.argv[1]) )
 print("Note: Enter 'q' to terminate connections")
 while True:
+    print("Server listening on port # " + str(sys.argv[1]) )
+    print("Note: Enter 'q' to terminate connections")
     first_peer, client_information = server_socket.accept()
     clients_list.append(client_information)
     if os.fork() == 0 :
@@ -91,5 +65,32 @@ while True:
                 except socket.error as msg:
                     print(f'Socket Error: {msg}')
 
-            if mode == 'wait':
+            elif mode == 'wait':
                 first_peer.send(bytes(str(client_information[0]), "utf-8"))
+
+            elif mode == 'file upload':
+                file_name = first_peer.recv(1024).decode("utf-8")
+                file_data = ""
+                first_peer.settimeout(5) # setting recv timeout
+                while True:
+                    try: 
+                        data = first_peer.recv(1024).decode("utf-8")
+                        file_data = file_data + data
+                    except socket.timeout:  # run when recv timeout expires
+                        break
+                first_peer.settimeout(None)
+                with open("server_files/"+file_name, 'w') as file:
+                    file.write(file_data)
+                    file.close()
+                    os.system('clear')
+
+            elif mode == 'file download':
+                file_name = first_peer.recv(1024).decode("utf-8")
+                with open("server_files/"+file_name, "r") as file:
+                    file_data = file.read()
+                    first_peer.send(bytes(file_data, "utf-8"))
+                    print(f'...Sending file to client {client_information[0]} {client_information[1]}...')
+                    time.sleep(5)
+                    print(f'{file_name} is sent successfully')
+                    file.close()
+                    os.system('clear')
